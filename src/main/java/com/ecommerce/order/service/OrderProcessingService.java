c74d7db06c128810e751dc0d1e1030d0f948dc3c
ranjan <ranjan@example.com>
2025-03-19 21:41:57 +0000

    Added retry and processing logic

--- src/main/java/com/ecommerce/order/service/OrderProcessingService.java
+++ src/main/java/com/ecommerce/order/service/OrderProcessingService.java
@@ -3,11 +3,20 @@ package com.ecommerce.order.service;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ecommerce.order.dao.OrderRepository;
 import com.ecommerce.order.dto.Order;
+import com.ecommerce.order.entity.OrderStatus;
+import com.ecommerce.order.entity.PaymentStatus;
+import com.ecommerce.order.exception.OrderNotFoundException;
 import com.stripe.exception.StripeException;
 
+import jakarta.persistence.OptimisticLockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,22 +28,66 @@ public class OrderProcessingService {
     @Autowired
     private StripeService stripeService;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
+    private final String ORDER_PAYMENT_SUCCESS="succeeded";
+    private final String ORDER_PAYMENT_FAILED="requires_payment_method";
+    private final String CUSTOMER_TOPIC="customer";
+    private final String VENDOR_TOPIC="vendor";
+
+    @Autowired
+    private KafkaTemplate<String,Object> kafkaTemplate;
+
     @KafkaListener(topics = "order",groupId="order-processing-group")
-    public void consume(ConsumerRecord<String,Object> record) {
-        try{
-            Order order=(Order) record.value();
-            stripeService.createPaymentIntent(order);
-        }catch(StripeException ex){
+    @Retryable(
+        value = OptimisticLockException.class, 
+        maxAttempts = 3, // Retry up to 3 times
+        backoff = @Backoff(delay = 500) // Wait 500ms before retrying
+    )
+    @Transactional
+    public void consume(ConsumerRecord<String,Object> record) throws StripeException,OrderNotFoundException {
 
-        }
+       
+            String status="";
         
+            Order order=(Order) record.value();
 
-        //get the value from record and call stripe service for payment
-        //if payment successfuly done
-            // send a notification to vendor with order details
-            // send a notification to customer by email
+            status=stripeService.createPaymentIntent(order);
+
+            com.ecommerce.order.entity.Order currentOrder=orderRepository.findById(order.getOrderId())
+                                                 .orElseThrow( () ->  new OrderNotFoundException("No order found for the provided order id::"));
+
+            if(status!=null && status.equals(ORDER_PAYMENT_SUCCESS)){
+               
+                currentOrder.setOrderStatus(OrderStatus.PROCESSING);
+                currentOrder.setPaymentStatus(PaymentStatus.PAID);
+                orderRepository.save(currentOrder);
+
+                //Use Kafka to send notification to notification service to vendor (vendor group)
+                kafkaTemplate.send(VENDOR_TOPIC, currentOrder);
+
+
+                //Use kafka to send notification to notification service to customer (customer group)
+                kafkaTemplate.send(CUSTOMER_TOPIC, currentOrder);
+                                
+            }
+            else if(status!=null && status.equals(ORDER_PAYMENT_FAILED)){
+                currentOrder.setOrderStatus(OrderStatus.CANCELLED);
+                currentOrder.setPaymentStatus(PaymentStatus.FAILED);
+                orderRepository.save(currentOrder);
+                //Use kafka to send notification to notification service to customer (customer group)
+                kafkaTemplate.send(CUSTOMER_TOPIC, currentOrder);
+            }
+            else{
+                System.out.println("Order status recieved for which no handling is available"+status);
+            }
+
+        
+        
         //else
             //send a failed notification to customer with stripe error message    
     }
+    
     
 }
